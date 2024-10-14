e3a5c6763cc9d9c1b2ed13d9c9412b9e1a8d5d97
SirLuxord <haendel.tee.2013@gmail.com>
2024-10-14 22:30:33 +0100

    Cambios en la vista y terminada la funcionalidad de enviar emails

--- src/main/java/dad/enviaremail/RootController.java
+++ src/main/java/dad/enviaremail/RootController.java
@@ -4,16 +4,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RootController implements Initializable {
+
+
 
     public RootController() {
         try{
@@ -61,17 +65,44 @@ public class RootController implements Initializable {
 
     @FXML
     void onEnviarAction(ActionEvent event) {
-
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName(nombreIPTextField.getText());
+            email.setSmtpPort(Integer.parseInt(puertoTextField.getText()));
+            email.setAuthenticator(new DefaultAuthenticator(remitenteTextField.getText(), contraseñaPassField.getText()));
+            email.setSSLOnConnect(smtpCheckBox.isSelected());
+            email.setFrom(remitenteTextField.getText());
+            email.setSubject(asuntoTextField.getText());
+            email.setMsg(MensajeTextArea.getText());
+            email.addTo(emailTextField.getText());
+            email.send();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Mensaje enviado");
+            alert.setHeaderText("Mensaje enviado con éxito a " + emailTextField.getText());
+            alert.showAndWait();
+        } catch (EmailException e) {
+            System.out.println("No se ha podido enviar el email");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No se pudo enviar el email");
+            alert.setContentText("Invalid message supplied");
+            alert.showAndWait();
+        }
     }
 
     @FXML
-    void onServidorTrue(ActionEvent event) {
-
-    }
+    private CheckBox smtpCheckBox;
 
     @FXML
     void onVaciarAction(ActionEvent event) {
-
+        MensajeTextArea.setText("");
+        asuntoTextField.setText("");
+        contraseñaPassField.setText("");
+        emailTextField.setText("");
+        nombreIPTextField.setText("");
+        puertoTextField.setText("");
+        remitenteTextField.setText("");
+        smtpCheckBox.setSelected(false);
     }
 
     public GridPane getRoot() {
